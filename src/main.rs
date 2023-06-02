e77616910312d2e9b985466f0930c7f17d62dcf2
KuengNoel <74761380+KuengNoel@users.noreply.github.com>
2023-06-02 15:10:52 +0200

    5 handle stdout errors (#6)
    * Done!
    
    * Done!
    
    * Update Cargo.toml and Cargo.lock

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,36 @@ use std::error::Error;
 
 use cluster::logger::Logger;
 use cluster::settings::Settings;
+use tokio::task;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     println!("{}", Settings::logging_mode());
     println!("{}", Settings::host_address());
 
-    Logger::info("This is a info");
-    Logger::warning("This is a warning");
+    let task_01 = task::spawn(async {
+        Logger::info("A").await;
+    });
+
+    let task_02 = task::spawn(async {
+        Logger::info("B").await;
+    });
+
+    let task_03 = task::spawn(async {
+        Logger::info("C").await;
+    });
+
+    let task_04 = task::spawn(async {
+        Logger::info("D").await;
+    });
+
+    Logger::info("This is a info").await;
+    Logger::warning("This is a warning").await;
+
+    task_01.await?;
+    task_02.await?;
+    task_03.await?;
+    task_04.await?;
+
     Ok(())
 }
